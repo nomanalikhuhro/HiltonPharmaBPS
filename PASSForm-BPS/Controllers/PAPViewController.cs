bd4d2aa8e99208de745e41932c974d9ace7f8947
Out Dev1 <out.dev1@hiltonpharma.com>
2024-03-29 15:56:59 +0500

    Pap changes

---
+++ PASSForm-BPS/Controllers/PAPViewController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PASSForm_BPS.Controllers
+{
+    public class PAPViewController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Submitted()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create(string RequestId, string PAPType)
+        {
+            if (RequestId == null)
+            {
+                return PartialView("Create_PartialView");
+            }
+            else
+            {
+                return View();
+             }
+
+    }
+    }
+}
---
+++ PASSForm-BPS/wwwroot/js/PAPScripts.js
@@ -0,0 +1,36 @@
+﻿function nextButtonClick() {
+   
+
+    var reqid = document.getElementById('RequestId').value;
+    var selectedOption = document.querySelector('input[name="options"]:checked');
+    var selectedOptionValue = selectedOption ? selectedOption.value : null;
+
+    if (selectedOptionValue !== null) {
+        $.ajax({
+            url: "/PAPView/Create", // Replace with your controller and action names
+            method: "GET", // Use GET or POST based on your server's requirements
+            data: { RequestId: reqid, PAPType: selectedOptionValue }, // Send the unique identifier as data
+            success: function (data) {
+                $('body').html(data);
+            },
+            error: function (xhr, status, error) {
+                console.error("Error:", status, error);
+            }
+        });
+    } else {
+
+        Swal.fire({
+            icon: "info",
+            title: 'Select Options Available!!',
+            showConfirmButton: false,
+            timer: 1500,
+            width: 380,
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            customClass: {
+                title: 'small-font',
+                icon: 'small-icon'
+            }
+        });
+    }
+}
