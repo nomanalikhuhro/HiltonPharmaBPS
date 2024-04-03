72c93ed6d62b9bc240b824408e55453fbc84ffd2
Out Dev1 <out.dev1@hiltonpharma.com>
2024-04-03 15:50:00 +0500

    3-4-2024

--- PASSForm-BPS/Controllers/PAPViewController.cs
+++ PASSForm-BPS/Controllers/PAPViewController.cs
@@ -1,9 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MySql.Data.MySqlClient;
+using PASSForm_BPS.Models;
+using PASSForm_BPS.ViewModel;
+using System.Data;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace PASSForm_BPS.Controllers
 {
     public class PAPViewController : Controller
     {
+        private readonly PassDbContext _passDbContext;
+        private readonly TestSalesDbContext _testSalesDbContext;
+        private readonly string _connectionString;
+
+        public string _sqlconnection;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        DataTable dt_HCPDETAILS = new DataTable();
+        DataTable dt_HCPDOCS = new DataTable();
+
+        public PAPViewController(PassDbContext passDbContext, TestSalesDbContext testSalesDbContext, IConfiguration configuration1, IWebHostEnvironment webHostEnvironment)
+        {
+            this._passDbContext = passDbContext;
+            this._testSalesDbContext = testSalesDbContext;
+            _webHostEnvironment = webHostEnvironment;
+            _sqlconnection = configuration1.GetConnectionString("SqlConnection");
+            _connectionString = configuration1.GetConnectionString("MySqlServerConnection");
+            /* _sqlconnection = "Server=192.168.10.6;Database=DSRvsMREP;User ID=test2;Password=abc123+;Integrated Security=False;Trusted_Connection=False;Encrypt=False;";*/// configuration1.GetConnectionString("SqlConnection"); // configuration1.GetConnectionString["SqlConnection"];
+        }
         public IActionResult Index()
         {
             return View();
@@ -23,9 +48,82 @@ namespace PASSForm_BPS.Controllers
             }
             else
             {
-                return View();
+                var Empid_SessionValue = HttpContext.Session.GetString("EmpIdbps");
+                ViewBag.PapType = PAPType;
+                var param1 = "44-EQU-2024";
+
+                var distributor = _passDbContext.DisterMappings.FromSqlRaw("call sp_GetDistributerDetails(@Tracking_ID)"
+                                                , new MySqlParameter("@Tracking_ID", param1)).ToList();
+
+                var combinedViewModel = new BPSRequestListViewModel
+                {
+
+                    disterMappings = distributor,
+
+                };
+                return View(combinedViewModel);
              }
 
     }
+
+        [HttpGet]
+        public object GetMacrobrick(string disValue)
+        {
+            try
+            {
+
+
+                var macrobrickrecords = _passDbContext.DisMacMappings.FromSqlRaw("call sp_GetMacroBrickDetails(@DisCode)"
+    , new MySqlParameter("@DisCode", disValue)).ToList();
+
+                return Json(macrobrickrecords);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpGet]
+        public object GetChemist(string brickValue)
+        {
+            try
+            {
+                var chemrecords = _passDbContext.MacChemMappings.FromSqlRaw("call sp_ChemistRecords(@MacCode)"
+, new MySqlParameter("@MacCode", brickValue)).ToList();
+
+                string html = "<option value=\"Select\">Select</option>";
+                foreach (var items in chemrecords)
+                {
+
+
+                    var optionValue = $"{items.ChemistCode} - {items.ChemistName}";
+
+
+                    html += $"<label><input id=\"{items.ChemistCode}\" type=\"checkbox\" value=\"{optionValue}\">{optionValue}</label>";
+
+
+                }
+                var ChmeistBrichDEtails = new
+                {
+                    macChemMappings = html,
+                    countOfChemist = chemrecords.Count
+
+                };
+
+                return ChmeistBrichDEtails;
+            }
+            catch (Exception ex)
+            {
+
+                DateTime timestampValue = DateTime.Now;
+
+                GlobalClass.LogException(_passDbContext, ex, nameof(GetChemist), "Error message");
+                return View("ErrorView");
+            }
+
+        }
     }
 }
--- PASSForm-BPS/wwwroot/js/PAPScripts.js
+++ PASSForm-BPS/wwwroot/js/PAPScripts.js
@@ -34,3 +34,180 @@
         });
     }
 }
+
+
+
+function getmacrobrickbydiscode() {
+    var selecteddis = document.getElementById('distributer').value;
+    var parts = selecteddis.split('-');
+
+    // Extract the left part (the part before the '-')
+    var leftValue = parts[0].trim();
+
+    $.ajax({
+        url: "/PAPView/GetMacrobrick", // Replace with your controller and action names
+        method: "GET", // Use GET or POST based on your server's requirements
+        data: { disValue: leftValue }, // Send the unique identifier as data
+        success: function (data) {
+
+            $('#selectedbrick').empty();
+
+            // Add default option
+            $('#selectedbrick').append($('<option>', {
+                value: 'Select',
+                text: 'Select'
+            }));
+
+            // Populate options with brick code and name
+            $.each(data, function (index, item) {
+                $('#selectedbrick').append($('<option>', {
+                    value: item.macrobrickCode + ' - ' + item.macroBrickName,
+                    text: item.macrobrickCode + ' - ' + item.macroBrickName
+                }));
+            });
+
+        },
+        error: function (xhr, status, error) {
+            // Handle errors here
+            console.error("Error:", status, error);
+        }
+    });
+}
+
+function createaccordion() {
+
+
+
+    var checkboxes = document.querySelectorAll(".dropdown-content input[type='checkbox']");
+    checkedItems = [];
+    var checkboxindex = 0;
+    var html = '';
+
+    var checkbox = null;
+    var attrid;
+    checkboxes.forEach(function (checkboxS) {
+        if (checkboxS.checked && $('#tableAcc').find('[_id="' + checkboxS.id + '"]').length == 0) {
+
+            checkbox = checkboxS.value
+            attrid = checkboxS.id
+            checkboxindex = checkboxS.id;
+            //  checkedItems.push(checkbox.value);
+
+        }
+        if (checkboxS.checked) {
+
+        }
+        else {
+            $('#tableAcc').find('[_id="' + checkboxS.id + '"]').remove();
+            $('#tableAcc').find('[_idbtn="' + checkboxS.id + '"]').remove();
+        }
+
+    });
+
+
+    //  if (existingHtml.indexOf(`id="chemist-${checkboxindex}"`) === -1) {
+    if (checkbox != null) {
+        html += `
+
+
+            <button style="margin-top:2%;" id="chemist-${checkboxindex}" onclick="togglePanel(this)" _idbtn=${attrid} class="accordion">${checkbox}</button>
+            <div class="panel" id="ChemistPanelID-${checkboxindex}" _id=${attrid} style="height:auto;">
+               <div style="padding-top:15px; padding-left:15px;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-3">
+                        <label>From:</label>
+                        <input type="month" id="startdatepre-${checkboxindex}" value="" style="margin-left: 2%;" />
+                    </div>
+                    <div class="col-3">
+                        <label>To:</label>
+                        <input type="month" id="enddatepre-${checkboxindex}" value="" style="margin-left: 2%;" />
+                    </div>
+                    <div class="col-3">
+                         <input style="margin-top:2px;" id="search-${checkboxindex}" onclick="ActivitySales(${checkboxindex})" type="button" class="searchbutton" value="Search"  />  </div>
+                </div>
+            </div>
+        </div>
+     
+        </div>
+
+                <div id="pre-${checkboxindex}"> </div>
+               
+            </div>`;
+
+        loadPartialView(attrid);
+
+        $('#tableAcc').append(html);
+    }
+
+
+
+
+}
+
+function loadPartialView(attrid) {
+    // Replace 'your_partial_view_url' with the URL of your server-side endpoint that serves the partial view content
+    var url = '/PAPView/Accordion_PartialView';
+
+    $.get(url, function (data) {
+        $('#pre-' + attrid).html(data); // Insert partial view content into the div
+    });
+}
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+
+
+
+$(document).ready(function () {
+
+    // Initialize select2
+    $("#distributer").select2();
+    $("#selectedbrick").select2();
+
+});
+
+function getchembymacrobrickcode() {
+
+
+    var searchButtons = document.getElementsByClassName('searchbutton');
+    if (searchButtons.length > 0) {
+        var searchbutton = searchButtons[0];
+        searchbutton.removeAttribute('disabled');
+    }
+    var selectedbrick = document.getElementById('selectedbrick').value;
+    var parts = selectedbrick.split('-');
+
+    // Extract the left part (the part before the '-')
+    var leftValue = parts[0].trim();
+
+    $.ajax({
+        url: "/PAPView/GetChemist", // Replace with your controller and action names
+        method: "GET", // Use GET or POST based on your server's requirements
+        data: { brickValue: leftValue }, // Send the unique identifier as data
+        success: function (data) {
+            document.getElementById('selectedchemist').innerHTML = data.macChemMappings;
+            $('#tableAcc').empty();
+            // Handle the server's response here
+            // $("#tableAcc").html(data);
+        },
+        error: function (xhr, status, error) {
+            // Handle errors here
+            console.error("Error:", status, error);
+        }
+    });
+}
